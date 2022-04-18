75642a71703dd8b3f62e3da2548032d18e8b9b9b
unknown <johnsonevans686@gmail.com>
2022-04-18 20:15:57 +0100

    Routes add by Evans

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const routesArray = [
+    {
+      path: "/", rComponent: <p>Accueil</p>
+    },
+    {
+      path: "/about", rComponent: <p>A propos</p>
+    },
+    {
+      path: "/jobs", rComponent: <p>Jobs</p>
+    },
+    {
+      path: "/contact", rComponent: <p>Contact</p>
+    },
+    {
+      path: "/legales", rComponent: <p>Mentions legales</p>
+    },
+
+  ]
   return (
-    <div className="App">
-      Jobers
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {
+          routesArray.map((route, index) => <Route key={"route number" + index}
+            path={route.path}
+            element={route.rComponent} />)
+        }
+      </Routes>
+    </BrowserRouter>
   );
 }
 
